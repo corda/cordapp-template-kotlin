5370a45b9d08640d9ff64f45c607cc9915202d78
Mike Hearn <mike@plan99.net>
2016-11-07 15:09:04 +0100

    Update ExampleProtocol to latest API changes, remove dead code.

--- src/main/kotlin/com/example/protocol/ExampleProtocol.kt
+++ src/main/kotlin/com/example/protocol/ExampleProtocol.kt
@@ -5,15 +5,13 @@ import com.example.contract.ExampleContract
 import com.example.contract.ExampleState
 import com.example.model.ExampleModel
 import com.r3corda.core.crypto.Party
+import com.r3corda.core.node.PluginServiceHub
 import com.r3corda.core.protocols.ProtocolLogic
-import com.r3corda.node.services.api.ServiceHubInternal
 
 /**
  * This example shows a protocol sending and receiving data.
  */
 object ExampleProtocol {
-    data class OfferMessage(val notary: Party, val dealBeingOffered: ExampleState)
-
     /**
      * Initiates the agreement between the two parties
      */
@@ -26,18 +24,18 @@ object ExampleProtocol {
         }
     }
 
-    class Service(services: ServiceHubInternal) {
-        init {
-            services.registerProtocolInitiator(Requester::class, ::Receiver)
-        }
-    }
-
     class Receiver(val otherParty: Party): ProtocolLogic<ExampleState>() {
         @Suspendable
         override fun call(): ExampleState {
             val offer = receive<ExampleState>(otherParty).unwrap { it }
             send(otherParty, offer)
             return offer
+        }
+    }
+
+    class Service(services: PluginServiceHub) {
+        init {
+            services.registerProtocolInitiator(Requester::class, ::Receiver)
         }
     }
 }
