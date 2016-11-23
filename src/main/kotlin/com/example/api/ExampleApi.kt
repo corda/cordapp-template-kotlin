6a4adc751f769ec13c2d343caea326302f4cf0d6
RogerWillis <roger.willis@r3cev.com>
2016-11-23 12:56:39 +0000

    * Flow -> Protocol

--- src/main/kotlin/com/example/api/ExampleApi.kt
+++ src/main/kotlin/com/example/api/ExampleApi.kt
@@ -3,7 +3,7 @@ package com.example.api
 import com.example.contract.PurchaseOrderContract
 import com.example.contract.PurchaseOrderState
 import com.example.model.PurchaseOrder
-import com.example.protocol.ExampleFlow
+import com.example.flow.ExampleFlow
 import net.corda.core.node.ServiceHub
 import net.corda.core.node.services.linearHeadsOfType
 import net.corda.core.transactions.SignedTransaction
@@ -43,19 +43,14 @@ class ExampleApi(val services: ServiceHub) {
     fun getPurchaseOrders() = services.vaultService.linearHeadsOfType<PurchaseOrderState>()
 
     /**
-<<<<<<< 2dd5f78fe6a381c8eff43f833b2f20d83086c104
-     * This initiates a flow to agree a deal with the other party. Once the flow finishes it will
-     * have written this deal to the ledger.
-=======
-     * This should only be called from the 'buyer' node. It initiates a protocol to agree a purchase order with a
-     * seller. Once the protocol finishes it will have written the purchase order to ledger. Both the buyer and the
+     * This should only be called from the 'buyer' node. It initiates a flow to agree a purchase order with a
+     * seller. Once the flow finishes it will have written the purchase order to ledger. Both the buyer and the
      * seller will be able to see it when calling /api/example/purchase-orders on their respective nodes.
      *
      * This end-point takes a Party name parameter as part of the path. If the serving node can't find the other party
      * in its network map cache, it will return an HTTP bad request.
      *
-     * The protocol is invoked asynchronously. It returns a future when the protocol's call() method returns.
->>>>>>> First commit for updated corda-sdk.
+     * The flow is invoked asynchronously. It returns a future when the flow's call() method returns.
      */
     @PUT
     @Path("{party}/create-purchase-order")
@@ -64,11 +59,7 @@ class ExampleApi(val services: ServiceHub) {
         if(otherParty != null) {
             val state = PurchaseOrderState(po, services.myInfo.legalIdentity, otherParty, PurchaseOrderContract())
             // The line below blocks and waits for the future to resolve.
-<<<<<<< 2dd5f78fe6a381c8eff43f833b2f20d83086c104
-            services.invokeFlowAsync<ExampleState>(ExampleFlow.Requester::class.java, swap, otherParty).resultFuture.get()
-=======
-            services.invokeProtocolAsync<SignedTransaction>(ExampleFlow.Initiator::class.java, state, otherParty).resultFuture.get()
->>>>>>> First commit for updated corda-sdk.
+            services.invokeFlowAsync<SignedTransaction>(ExampleFlow.Initiator::class.java, state, otherParty).resultFuture.get()
             return Response.status(Response.Status.CREATED).build()
         } else {
             return Response.status(Response.Status.BAD_REQUEST).build()
