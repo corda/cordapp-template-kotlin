472729c9d52e23ba52b2547f0c1e319980558c83
Ross Nicoll <ross.nicoll@r3.com>
2016-12-21 16:24:24 +0000

    Add simple test case for purchase order contract (#12)
    Adds test cases for purchase order contract.

---
+++ kotlin/src/test/kotlin/com/example/contract/PurchaseOrderTests.kt
@@ -0,0 +1,62 @@
+package com.example.contract
+
+import com.example.model.Address
+import com.example.model.Item
+import com.example.model.PurchaseOrder
+import net.corda.core.utilities.TEST_TX_TIME
+import net.corda.testing.*
+import org.junit.Test
+import java.time.Duration
+import java.time.temporal.TemporalAmount
+import java.util.*
+
+class PurchaseOrderTests {
+    @Test
+    fun `place`() {
+        val address = Address("London", "UK")
+        val items = listOf(Item("Hammer", 1))
+        val deliveryTime = TEST_TX_TIME.plus(Duration.ofDays(7))
+        val purchaseOrder = PurchaseOrder(1, Date(deliveryTime.toEpochMilli()), address, items)
+        ledger {
+            transaction {
+                output { PurchaseOrderState(purchaseOrder, MINI_CORP, MEGA_CORP, PurchaseOrderContract()) }
+                timestamp(TEST_TX_TIME)
+                `fails with`("Required com.example.contract.PurchaseOrderContract.Commands.Place command")
+                command(MEGA_CORP_PUBKEY, MINI_CORP_PUBKEY) { PurchaseOrderContract.Commands.Place() }
+                verifies()
+            }
+        }
+    }
+
+    @Test
+    fun `cannot place empty orders`() {
+        val address = Address("London", "UK")
+        val items = emptyList<Item>()
+        val deliveryTime = TEST_TX_TIME.plus(Duration.ofDays(7))
+        val purchaseOrder = PurchaseOrder(1, Date(deliveryTime.toEpochMilli()), address, items)
+        ledger {
+            transaction {
+                output { PurchaseOrderState(purchaseOrder, MINI_CORP, MEGA_CORP, PurchaseOrderContract()) }
+                timestamp(TEST_TX_TIME)
+                command(MEGA_CORP_PUBKEY, MINI_CORP_PUBKEY) { PurchaseOrderContract.Commands.Place() }
+                `fails with`("must order at least one type of item")
+            }
+        }
+    }
+
+    @Test
+    fun `cannot place historical orders`() {
+        val address = Address("London", "UK")
+        val items = listOf(Item("Hammer", 1))
+        val deliveryTime = TEST_TX_TIME.minus(Duration.ofDays(7))
+        val purchaseOrder = PurchaseOrder(1, Date(deliveryTime.toEpochMilli()), address, items)
+        ledger {
+            transaction {
+                output { PurchaseOrderState(purchaseOrder, MINI_CORP, MEGA_CORP, PurchaseOrderContract()) }
+                timestamp(TEST_TX_TIME)
+                command(MEGA_CORP_PUBKEY, MINI_CORP_PUBKEY) { PurchaseOrderContract.Commands.Place() }
+                `fails with`("delivery date must be in the future")
+            }
+        }
+    }
+}
