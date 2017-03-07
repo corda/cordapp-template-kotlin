89b187d114b249b22ac0e04b6ad877e9bdc7d3ac
Ross Nicoll <ross.nicoll@r3.com>
2017-03-07 18:02:06 +0000

    Add SSL config to example client RPC

--- java-source/src/main/java/com/template/client/TemplateClientRPC.java
+++ java-source/src/main/java/com/template/client/TemplateClientRPC.java
@@ -3,6 +3,7 @@ package com.template.client;
 import com.google.common.net.HostAndPort;
 import com.template.state.TemplateState;
 import kotlin.Pair;
+import net.corda.core.crypto.CryptoUtilities;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.node.services.config.ConfigUtilities;
@@ -27,7 +28,7 @@ public class TemplateClientRPC {
 
         final Logger logger = LoggerFactory.getLogger(TemplateClientRPC.class);
         final HostAndPort nodeAddress = HostAndPort.fromString(args[0]);
-        final CordaRPCClient client = new CordaRPCClient(nodeAddress, null, null);
+        final CordaRPCClient client = new CordaRPCClient(nodeAddress, ConfigUtilities.configureTestSSL(), null);
 
         // Can be amended in the Main file.
         client.start("user1", "test");
