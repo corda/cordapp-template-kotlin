09449529579697337d3734c9c91ef600cc8d50a0
Maksymilian Pawlak <120831+m4ksio@users.noreply.github.com>
2017-09-22 17:16:49 +0100

    Remove obsolete imports

--- src/test/kotlin/com/template/Main.kt
+++ src/test/kotlin/com/template/Main.kt
@@ -3,7 +3,6 @@ package com.template
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.getOrThrow
 import net.corda.node.services.transactions.ValidatingNotaryService
-import net.corda.nodeapi.ServiceInfo
 import net.corda.nodeapi.User
 import net.corda.nodeapi.internal.ServiceInfo
 import net.corda.testing.driver.driver
