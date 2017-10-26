aec6097ce3fa8840e948ca9a0a4baee877c1e724
joeldudleyr3 <joel.dudley@r3.com>
2017-10-26 13:40:54 +0100

    Splits states and contracts from other source files so that only shared structures have to be sent over the wire.

--- other-sources/src/main/kotlin/com/template/App.kt
+++ other-sources/src/main/kotlin/com/template/App.kt
@@ -31,32 +31,6 @@ class TemplateApi(val rpcOps: CordaRPCOps) {
     }
 }
 
-// *****************
-// * Contract Code *
-// *****************
-// This is used to identify our contract when building a transaction
-val TEMPLATE_CONTRACT_ID = "com.template.TemplateContract"
-
-open class TemplateContract : Contract {
-    // A transaction is considered valid if the verify() function of the contract of each of the transaction's input
-    // and output states does not throw an exception.
-    override fun verify(tx: LedgerTransaction) {
-        // Verification logic goes here.
-    }
-
-    // Used to indicate the transaction's intent.
-    interface Commands : CommandData {
-        class Action : Commands
-    }
-}
-
-// *********
-// * State *
-// *********
-data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> get() = listOf()
-}
-
 // *********
 // * Flows *
 // *********
