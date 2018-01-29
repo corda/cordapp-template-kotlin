2d94dcb7cd5cda4937fcc8e2e16ea32307c45c50
joeldudleyr3 <joel.dudley@r3.com>
2018-01-29 16:30:49 +0000

    Adds webserver integration test. Clears up other integration test.

--- cordapp/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
+++ cordapp/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
@@ -7,28 +7,58 @@ import net.corda.testing.DUMMY_BANK_A
 import net.corda.testing.DUMMY_BANK_B
 import net.corda.testing.DUMMY_NOTARY
 import net.corda.testing.driver.driver
-import org.junit.Assert
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class DriverBasedTest {
+    val nodeNames = listOf(DUMMY_NOTARY.name, DUMMY_BANK_A.name, DUMMY_BANK_B.name)
+
     @Test
-    fun `run driver test`() {
+    fun `node test`() {
         driver(isDebug = true, startNodesInProcess = true) {
             // This starts three nodes simultaneously with startNode, which returns a future that completes when the node
             // has completed startup. Then these are all resolved with getOrThrow which returns the NodeHandle list.
             val (notaryHandle, partyAHandle, partyBHandle) = listOf(
-                    startNode(providedName = DUMMY_NOTARY.name, advertisedServices = setOf(ServiceInfo(SimpleNotaryService.type))),
-                    startNode(providedName = DUMMY_BANK_A.name),
-                    startNode(providedName = DUMMY_BANK_B.name)
+                    startNode(providedName = nodeNames[0], advertisedServices = setOf(ServiceInfo(SimpleNotaryService.type))),
+                    startNode(providedName = nodeNames[1]),
+                    startNode(providedName = nodeNames[2])
             ).map { it.getOrThrow() }
 
-            // This test will call via the RPC proxy to find a party of another node to verify that the nodes have
-            // started and can communicate. This is a very basic test, in practice tests would be starting flows,
-            // and verifying the states in the vault and other important metrics to ensure that your CorDapp is working
-            // as intended.
-            Assert.assertEquals(notaryHandle.rpc.wellKnownPartyFromX500Name(DUMMY_BANK_A.name)!!.name, DUMMY_BANK_A.name)
-            Assert.assertEquals(partyAHandle.rpc.wellKnownPartyFromX500Name(DUMMY_BANK_B.name)!!.name, DUMMY_BANK_B.name)
-            Assert.assertEquals(partyBHandle.rpc.wellKnownPartyFromX500Name(DUMMY_NOTARY.name)!!.name, DUMMY_NOTARY.name)
+            // This test makes an RPC call to retrieve another node's name from the network map, to verify that the
+            // nodes have started and can communicate. This is a very basic test, in practice tests would be starting
+            // flows, and verifying the states in the vault and other important metrics to ensure that your CorDapp is
+            // working as intended.
+            assertEquals(notaryHandle.rpc.wellKnownPartyFromX500Name(DUMMY_BANK_A.name)!!.name, DUMMY_BANK_A.name)
+            assertEquals(partyAHandle.rpc.wellKnownPartyFromX500Name(DUMMY_BANK_B.name)!!.name, DUMMY_BANK_B.name)
+            assertEquals(partyBHandle.rpc.wellKnownPartyFromX500Name(DUMMY_NOTARY.name)!!.name, DUMMY_NOTARY.name)
+        }
+    }
+
+    @Test
+    fun `node webserver test`() {
+        driver(isDebug = true, startNodesInProcess = true) {
+            val nodeHandles = listOf(
+                    startNode(providedName = nodeNames[0], advertisedServices = setOf(ServiceInfo(SimpleNotaryService.type))),
+                    startNode(providedName = nodeNames[1]),
+                    startNode(providedName = nodeNames[2])
+            ).map { it.getOrThrow() }
+
+            // This test starts each node's webserver and makes an HTTP call to retrieve the body of a GET endpoint on
+            // the node's webserver, to verify that the nodes' webservers have started and have loaded the API.
+            nodeHandles.forEach { nodeHandle ->
+                startWebserver(nodeHandle).getOrThrow()
+
+                val nodeAddress = nodeHandle.webAddress
+                val url = "http://$nodeAddress/api/template/templateGetEndpoint"
+
+                val request = Request.Builder().url(url).build()
+                val client = OkHttpClient()
+                val response = client.newCall(request).execute()
+
+                assertEquals("Template GET endpoint.", response.body().string())
+            }
         }
     }
 }
