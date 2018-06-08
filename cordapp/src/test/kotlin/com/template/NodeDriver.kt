ff3c898039d92c88c3000f9cb273e7f566cb4cfb
Joel Dudley <joel.dudley@r3cev.com>
2018-06-08 15:59:53 +0100

    Update NodeDriver.kt

--- cordapp/src/test/kotlin/com/template/NodeDriver.kt
+++ cordapp/src/test/kotlin/com/template/NodeDriver.kt
@@ -21,7 +21,7 @@ import net.corda.testing.node.User
  */
 fun main(args: Array<String>) {
     // No permissions required as we are not invoking flows.
-    val user = User("user1", "test", permissions = setOf())
+    val user = User("user1", "test", permissions = setOf("ALL"))
     driver(DriverParameters(isDebug = true, waitForAllNodesToFinish = true)) {
         val (partyA, partyB) = listOf(
                 startNode(providedName = CordaX500Name("PartyA", "London", "GB"), rpcUsers = listOf(user)),
