ecd326c76be4b22a280f2aa1055b1e647f19310f
joeldudleyr3 <joel.dudley@r3.com>
2018-08-09 09:22:49 +0100

    Removes unnecessary getter.

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -30,5 +30,5 @@ class TemplateContract : Contract {
 // * State *
 // *********
 data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> get() = listOf()
+    override val participants: List<AbstractParty> = listOf()
 }
