f971a302937bbce3c13b9082f54c3e3b09ab558b
Joel Dudley <joel.dudley@r3cev.com>
2018-10-22 07:59:02 +0200

    Update StatesAndContracts.kt

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -10,10 +10,11 @@ import net.corda.core.transactions.LedgerTransaction
 // * Contract Code *
 // *****************
 class TemplateContract : Contract {
-    // This is used to identify our contract when building a transaction
+    // This is used to identify our contract when building a transaction.
     companion object {
         val ID = "com.template.TemplateContract"
     }
+    
     // A transaction is considered valid if the verify() function of the contract of each of the transaction's input
     // and output states does not throw an exception.
     override fun verify(tx: LedgerTransaction) {
