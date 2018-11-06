1ed7a4b4e0dbc2a4c4889ee4c8b9ab7556b1e044
joeldudleyr3 <joel.dudley@r3.com>
2018-11-06 15:33:05 +0000

    Unneeded whitespace.

--- clients/src/main/kotlin/com/template/webserver/Server.kt
+++ clients/src/main/kotlin/com/template/webserver/Server.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType.SERVLET
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-
 /**
  * Our Spring Boot application.
  */
