8d33d212c8d00197ba1a4ff8aab5f9deb5440bde
Florian Friemel <florian.friemel@r3.com>
2018-12-19 16:31:34 +0000

    Add BelongsToContract annotation.

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -1,5 +1,6 @@
 package com.template
 
+import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.contracts.ContractState
@@ -30,6 +31,5 @@ class TemplateContract : Contract {
 // *********
 // * State *
 // *********
-data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> = listOf()
-}
+@BelongsToContract(TemplateContract::class)
+data class TemplateState(val data: String, override val participants: List<AbstractParty> = listOf()) : ContractState
