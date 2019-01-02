c1f274464bcee00d81c922dc042cb8911e114185
Florian Friemel <florian.friemel@r3.com>
2019-01-02 16:13:36 +0000

    Move ContractTests to the contracts module.

---
+++ contracts/src/test/kotlin/com/template/ContractTests.kt
@@ -0,0 +1,13 @@
+package com.template
+
+import net.corda.testing.node.MockServices
+import org.junit.Test
+
+class ContractTests {
+    private val ledgerServices = MockServices()
+
+    @Test
+    fun `dummy test`() {
+
+    }
+}
