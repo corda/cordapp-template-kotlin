8f75ab2558a24baca50c3b2031c0bd0232ad084a
RogerWillis <roger.willis@r3cev.com>
2019-02-13 16:28:50 +0000

    Fixed build.gradle's to correctly add token SDK as a dependency.

--- contracts/src/main/kotlin/com/template/states/TemplateState.kt
+++ contracts/src/main/kotlin/com/template/states/TemplateState.kt
@@ -1,12 +1,21 @@
 package com.template.states
 
+import com.r3.corda.sdk.token.contracts.states.EvolvableToken
 import com.template.contracts.TemplateContract
 import net.corda.core.contracts.BelongsToContract
-import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import java.math.BigDecimal
 
 // *********
 // * State *
 // *********
 @BelongsToContract(TemplateContract::class)
-data class TemplateState(val data: String, override val participants: List<AbstractParty> = listOf()) : ContractState
+data class SomeToken(
+        val stuff: String,
+        val maintainer: Party,
+        override val displayTokenSize: BigDecimal = BigDecimal.ONE,
+        override val linearId: UniqueIdentifier = UniqueIdentifier()
+) : EvolvableToken() {
+    override val maintainers: List<Party> get() = listOf(maintainer)
+}
