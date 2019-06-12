3c578a9f6b8734274d2f0d42c86b8e8e77e1f2ce
Summer Smith <summer.smith497@gmail.com>
2019-06-12 15:28:00 -0700

    Delete DIDClaimPresentation.kt

--- contracts/src/test/kotlin/com/template/contracts/DIDClaimPresentation.kt
+++
@@ -1,8 +0,0 @@
-package com.template.contracts
-
-data class DIDClaimPresentation (
-        val context : List<String>,
-        val type : List<String>,
-        val verifiableCredential: List<Map<Any, Any>>,
-        val proof : Any
-)
