600ce6b1f7b8d754119ee519dbaba9b9f66501c8
Dhru Devalia <dhru.devalia@r3.com>
2019-07-04 09:33:14 +0100

    Revert "added states design"
    This reverts commit d0d12ed5d31dbfcecb3410126e6ff3b6f6ffcb82.

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -2,29 +2,19 @@ package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.flows.*
-import net.corda.core.identity.Party
-import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
-import java.nio.ByteBuffer
 
 // *********
 // * Flows *
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator(val messageSize : Int, val toSendTo : Party) : FlowLogic<Unit>() {
+class Initiator : FlowLogic<Unit>() {
     override val progressTracker = ProgressTracker()
-
 
     @Suspendable
     override fun call() {
         // Initiator flow logic goes here.
-        val bb = ByteBuffer.allocate(4)
-        bb.putInt(messageSize)
-        val byteArray = bb.array()
-        val counterParty = initiateFlow(toSendTo)
-
-
     }
 }
 
