b988d297c5335d981d9115e15fe92365f4e7baee
htouloupas <harrytouloupas@gmail.com>
2019-09-26 23:53:23 +0300

    Changed build.gradle to include some things mentioned in the token-sdk page on github. Also made a whole new Controller to include new endpoints.

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -1,9 +1,16 @@
 package com.template.webserver
 
+import com.template.flows.ExampleFlowWithFixedToken
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.Party
+import net.corda.core.messaging.vaultQueryBy
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 /**
  * Define your API endpoints here.
@@ -18,8 +25,36 @@ class Controller(rpc: NodeRPCConnection) {
 
     private val proxy = rpc.proxy
 
-    @GetMapping(value = "/templateendpoint", produces = arrayOf("text/plain"))
-    private fun templateendpoint(): String {
-        return "Define an endpoint here."
+    @PostMapping(value = "/tokens", produces = arrayOf("text/plain"))
+    private fun createToken(): String {
+        val recipient = proxy.networkMapSnapshot().flatMap { it.legalIdentities }[1]
+        return ExampleFlowWithFixedToken("GBP", 100, recipient).toString()
     }
+
+    @GetMapping(value = "/status", produces = arrayOf("text/plain"))
+    private fun status() = "200"
+
+    @GetMapping(value = "/servertime", produces = arrayOf("text/plain"))
+    private fun serverTime() = LocalDateTime.ofInstant(proxy.currentNodeTime(), ZoneId.of("UTC")).toString()
+
+    @GetMapping(value = "/addresses", produces = arrayOf("text/plain"))
+    private fun addresses() = proxy.nodeInfo().addresses.toString()
+
+    @GetMapping(value = "/identities", produces = arrayOf("text/plain"))
+    private fun identities() = proxy.nodeInfo().legalIdentities.toString()
+
+    @GetMapping(value = "/platformversion", produces = arrayOf("text/plain"))
+    private fun platformVersion() = proxy.nodeInfo().platformVersion.toString()
+
+    @GetMapping(value = "/peers", produces = arrayOf("text/plain"))
+    private fun peers() = proxy.networkMapSnapshot().flatMap { it.legalIdentities }.toString()
+
+    @GetMapping(value = "/notaries", produces = arrayOf("text/plain"))
+    private fun notaries() = proxy.notaryIdentities().toString()
+
+    @GetMapping(value = "/flows", produces = arrayOf("text/plain"))
+    private fun flows() = proxy.registeredFlows().toString()
+
+    @GetMapping(value = "/states", produces = arrayOf("text/plain"))
+    private fun states() = proxy.vaultQueryBy<ContractState>().states.toString()
 }
