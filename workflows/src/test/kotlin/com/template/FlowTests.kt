30f328e1faf65f8d1a39661f409aacb368d43610
peterli-r3 <51169685+peterli-r3@users.noreply.github.com>
2020-11-24 13:51:37 -0500

    gradle version bump

--- workflows/src/test/kotlin/com/template/FlowTests.kt
+++ workflows/src/test/kotlin/com/template/FlowTests.kt
@@ -1,33 +1,41 @@
 package com.template
 
 import com.template.flows.Responder
+import net.corda.core.identity.CordaX500Name
+import net.corda.testing.core.internal.ContractJarTestUtils.makeTestJar
 import net.corda.testing.node.MockNetwork
-import net.corda.testing.node.MockNetworkParameters
-import net.corda.testing.node.TestCordapp
+import net.corda.testing.node.MockNetworkNotarySpec
+import net.corda.testing.node.MockNodeParameters
+import net.corda.testing.node.StartedMockNode
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
-class FlowTests {
-    private val network = MockNetwork(MockNetworkParameters(cordappsForAllNodes = listOf(
-        TestCordapp.findCordapp("com.template.contracts"),
-        TestCordapp.findCordapp("com.template.flows")
-    )))
-    private val a = network.createNode()
-    private val b = network.createNode()
 
-    init {
-        listOf(a, b).forEach {
-            it.registerInitiatedFlow(Responder::class.java)
-        }
-    }
+class FlowTests {
+
+    lateinit var mockNetwork: MockNetwork
+    lateinit var a: StartedMockNode
+    lateinit var b: StartedMockNode
 
     @Before
-    fun setup() = network.runNetwork()
+    fun setup() {
+        mockNetwork = MockNetwork(
+                listOf("net.corda.training"),
+                notarySpecs = listOf(MockNetworkNotarySpec(CordaX500Name("Notary","London","GB")))
+        )
+        a = mockNetwork.createNode(MockNodeParameters())
+        b = mockNetwork.createNode(MockNodeParameters())
+        val startedNodes = arrayListOf(a, b)
+        // For real nodes this happens automatically, but we have to manually register the flow for tests
+        startedNodes.forEach { it.registerInitiatedFlow(Responder::class.java) }
+        mockNetwork.runNetwork()
+    }
 
     @After
-    fun tearDown() = network.stopNodes()
+    fun tearDown() {
 
+    }
     @Test
     fun `dummy test`() {
 
