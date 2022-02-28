f7d16a8b51ad9dbd0ccca80098ba59187eea2c78
peterli-r3 <peter.li@r3.com>
2022-02-28 14:22:20 +0800

    update flow test

--- workflows/src/test/kotlin/com/template/FlowTests.kt
+++ workflows/src/test/kotlin/com/template/FlowTests.kt
@@ -9,6 +9,7 @@ import java.util.concurrent.Future;
 import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import com.template.flows.Initiator
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.node.services.Vault.StateStatus
 
 
@@ -22,7 +23,8 @@ class FlowTests {
         network = MockNetwork(MockNetworkParameters(cordappsForAllNodes = listOf(
                 TestCordapp.findCordapp("com.template.contracts"),
                 TestCordapp.findCordapp("com.template.flows")
-        )))
+        ),
+            notarySpecs = listOf(MockNetworkNotarySpec(CordaX500Name("Notary","London","GB")))))
         a = network.createPartyNode()
         b = network.createPartyNode()
         network.runNetwork()
@@ -39,7 +41,7 @@ class FlowTests {
         network.runNetwork()
 
         //successful query means the state is stored at node b's vault. Flow went through.
-        // val inputCriteria: QueryCriteria = QueryCriteria.VaultQueryCriteria().withStatus(StateStatus.UNCONSUMED)
-        // val states = b.services.vaultService.queryBy(TemplateState::class.java, inputCriteria).states[0].state.data
+         val inputCriteria: QueryCriteria = QueryCriteria.VaultQueryCriteria().withStatus(StateStatus.UNCONSUMED)
+         val states = b.services.vaultService.queryBy(TemplateState::class.java, inputCriteria).states[0].state.data
     }
 }
